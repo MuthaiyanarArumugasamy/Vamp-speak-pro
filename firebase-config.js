ce9c443f6daa39cf6dc289fb5d827ef0e0412f48
MuthaiyanarArumugasamy <Priyadharshini23407@gmail.com>
2026-07-18 05:22:28 +0530

    Implement Firebase authentication features
    Added Firebase authentication functions for user registration, login, and logout. Implemented Google login and user data loading.

--- firebase-config.js
+++ firebase-config.js
@@ -116,9 +116,6 @@ getStorage(app);
 
 
 
-
-
-
 // Export Firebase Services
 
 export {
@@ -132,6 +129,380 @@ db,
 storage
 
 };
+
+/* ==========================================
+   Firebase Authentication Connection
+   Sprint 2 - Part 2
+========================================== */
+
+
+import {
+    auth,
+    db
+} from "./firebase-config.js";
+
+
+import {
+
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signOut,
+    onAuthStateChanged,
+    GoogleAuthProvider,
+    signInWithPopup
+
+}
+from 
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+
+
+import {
+
+    doc,
+    setDoc,
+    getDoc
+
+}
+from
+"https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+async function registerUser(
+email,
+password,
+name
+){
+
+try{
+
+
+const userCredential =
+await createUserWithEmailAndPassword(
+auth,
+email,
+password
+);
+
+
+
+const user =
+userCredential.user;
+
+
+
+await setDoc(
+
+doc(
+db,
+"users",
+user.uid
+),
+
+{
+
+
+name:name,
+
+email:email,
+
+role:"student",
+
+level:"Beginner",
+
+xp:0,
+
+coursesCompleted:0,
+
+createdAt:
+new Date()
+
+
+}
+
+);
+
+
+
+alert(
+"Registration Successful 🎉"
+);
+
+
+
+window.location.href=
+"index.html";
+
+
+
+}
+
+catch(error){
+
+
+alert(
+error.message
+);
+
+
+}
+
+
+
+}
+
+async function loginUser(
+email,
+password
+){
+
+
+try{
+
+
+const userCredential =
+await signInWithEmailAndPassword(
+
+auth,
+
+email,
+
+password
+
+);
+
+
+
+const user =
+userCredential.user;
+
+
+
+localStorage.setItem(
+
+"user",
+
+JSON.stringify({
+
+uid:user.uid,
+
+email:user.email
+
+})
+
+);
+
+
+
+alert(
+"Login Successful 🚀"
+);
+
+
+
+window.location.reload();
+
+
+
+}
+
+catch(error){
+
+
+alert(
+error.message
+);
+
+
+}
+
+
+}
+
+const googleProvider =
+new GoogleAuthProvider();
+
+
+
+async function googleLogin(){
+
+
+try{
+
+
+await signInWithPopup(
+
+auth,
+
+googleProvider
+
+);
+
+
+
+alert(
+"Google Login Successful"
+);
+
+
+
+window.location.reload();
+
+
+
+}
+
+catch(error){
+
+
+console.log(
+error.message
+);
+
+
+}
+
+
+}
+
+const googleBtn =
+document.getElementById(
+"googleLogin"
+);
+
+
+
+if(googleBtn){
+
+
+googleBtn.addEventListener(
+"click",
+googleLogin
+);
+
+
+}
+
+async function logoutUser(){
+
+
+try{
+
+
+await signOut(auth);
+
+
+
+localStorage.clear();
+
+
+
+alert(
+"Logged out successfully"
+);
+
+
+
+window.location.reload();
+
+
+
+}
+
+catch(error){
+
+
+console.log(error.message);
+
+
+}
+
+
+}
+
+onAuthStateChanged(
+auth,
+(user)=>{
+
+
+if(user){
+
+
+console.log(
+"User Logged In:",
+user.email
+);
+
+
+
+loadUserData(
+user.uid
+);
+
+
+
+}
+
+else{
+
+
+console.log(
+"No User Logged In"
+);
+
+
+}
+
+
+
+});
+
+async function loadUserData(uid){
+
+
+const userDoc =
+await getDoc(
+
+doc(
+db,
+"users",
+uid
+)
+
+);
+
+
+
+if(userDoc.exists()){
+
+
+const data =
+userDoc.data();
+
+
+
+const nameElement =
+document.getElementById(
+"userName"
+);
+
+
+
+if(nameElement){
+
+nameElement.innerText =
+data.name;
+
+}
+
+
+
+}
+
+
+
+}
+
+
 
 
 
